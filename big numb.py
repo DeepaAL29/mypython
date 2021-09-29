b814969256c3cc733bd73cfd494c8a9d79c8cf26
deepa <deepaashok5656@gmail.com>
2021-09-29 12:30:42 +0530

    commit

---
+++ big numb.py
@@ -0,0 +1,13 @@
+n1 = int(input("enter n1: "))
+n2 = int(input("enter n2: "))
+n3 = int(input("enter n3: "))
+
+def biggest(n1,n2,n3):
+    if(n1>n2) and (n1>n3):
+        return n1
+    elif (n2>n3) and (n2>n1):
+        return n2
+    else:
+        return n3
+
+print(biggest(n1,n2,n3))
